25313ed7cd9cbe523b5429afd9ed31091e4544b8
Slizh96 <slizh.olya@mail.ru>
2022-04-21 23:40:32 +0300

    zan 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import './App.css';
+import {Todolist} from "./Todolist";
 
 function App() {
+const tasks1=[
+    {id:1, title:'React', isDone:true},
+    {id:2, title:'Redux', isDone:false},
+    {id:3, title:'Css', isDone:true},
+]
+    const tasks2=[
+    {id:1, title:'React2', isDone:true},
+    {id:2, title:'Redux2', isDone:false},
+    {id:3, title:'Css2', isDone:true},
+]
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'What to bye'} tasks={tasks2}/>
         </div>
     );
 }
