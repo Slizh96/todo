f81c514e5dfe25c8cdfbba80fea59eacf201efc4
Slizh96 <slizh.olya@mail.ru>
2022-05-22 01:48:36 +0300

    stories app with redux done

--- src/stories/AppWithRedux.stories.tsx
+++ src/stories/AppWithRedux.stories.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import {ComponentStory, ComponentMeta} from '@storybook/react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
 import {action} from "@storybook/addon-actions";
-import {AddItemForm} from "../AddItemForm";
 import AppWithRedux from "../AppWithRedux";
+import {Provider} from "react-redux";
+import {store} from "../state/store";
+import {ReduxStoreProviderDecorator} from "../state/ReduxStoreProviderDecorator";
 
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
     title: 'TODOLIST/AppWithRedux',
     component: AppWithRedux,
+    decorators: [ReduxStoreProviderDecorator]
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        addTask: {
-            description: 'Button clicked inside form'
-        },
-    },
+
 } as ComponentMeta<typeof AppWithRedux>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux {...args} />;
+const Template: ComponentStory<typeof AppWithRedux> = (args) => <AppWithRedux  />;
 
-export const AppWithRedux = Template.bind({});
+export const AppWithReduxStory = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-AppWithRedux.args = {
-    addTask: action('Button clicked inside form')
+AppWithReduxStory.args = {
+
 };
